3a4bcaf1435ff30873b617facb9def75e5c2fbf5
Daniel Ale <danielale9291@gmail.com>
2022-07-03 09:05:18 +0100

    Create base app

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Blogs API')
+    .setDescription('The Blogs API description')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Blogs API Doc',
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api/doc', app, document, customOptions);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get('PORT'));
+}
+bootstrap();
