8b68aae13aa4eb25e3942b340674c32f60d63eba
Daniel Ale <danielale9291@gmail.com>
2022-07-04 17:20:01 +0100

    enable swagger documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from 'joi';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.string().required(),
+      }),
+      envFilePath: '../.env',
+    }),
+    PrismaModule,
+    UsersModule
+  ],
 })
 export class AppModule {}
